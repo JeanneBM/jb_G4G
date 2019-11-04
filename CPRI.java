46e87da77f78b8883938cd3aa8db95be61d15e2c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-04 23:44:03 +0100

    Create CPRI.java

---
+++ CPRI.java
@@ -0,0 +1,14 @@
+public class CPRI extends MSG {
+    void establishPhysicalLayer()
+    {
+        System.out.println("Physical Layer Established!");
+    }
+    void establishDataLinkLayer()
+    {
+        System.out.println("Data Link Layer Established!");
+    }
+    void establishApplicationLayer()
+    {
+        System.out.println("Application Layer Established!");
+    }
+}
