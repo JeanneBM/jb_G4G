33d4785352977401f37a3fca95b416eb4d6b59e9
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-04 23:43:28 +0100

    Create OBSAI.java

---
+++ OBSAI.java
@@ -0,0 +1,12 @@
+public class OBSAI extends MSG {
+    void establishPhysicalLayer(){
+        System.out.println("Physical Layer Established!");
+    };
+    void establishDataLinkLayer()
+    {
+        System.out.println("Data Link Layer Established!");
+    };
+    void establishApplicationLayer(){
+        System.out.println("Application Layer Established!");
+    };
+}
