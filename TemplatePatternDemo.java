0b405069952a243e86f26037b8440e3171f4db2c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-04 23:41:16 +0100

    Create TemplatePatternDemo.java

---
+++ TemplatePatternDemo.java
@@ -0,0 +1,9 @@
+public class TemplatePatternDemo {
+    public static void main (String[] args){
+        MSG msg = new OBSAI();
+        msg.print();
+        System.out.println();
+        msg = new CPRI();
+        msg.print();
+    }
+}
