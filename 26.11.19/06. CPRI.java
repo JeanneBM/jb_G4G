d6fbad194ac079a8137f3b041184975984a1b64a
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 19:32:06 +0100

    Create 06. CPRI.java

---
+++ 26.11.19/06. CPRI.java
@@ -0,0 +1,22 @@
+package MessagingProtocols;
+
+import MessagingProtocols.MSG;
+
+public class CPRI extends MSG {
+    @Override
+    public String establishPhysicalLayer() {
+        return "";
+    }
+
+    @Override
+    public String establishDataLinkLayer() {
+        return "";
+    }
+
+    @Override
+    public String establishApplicationLayer() {
+        return "";
+    }
+}
+
+
