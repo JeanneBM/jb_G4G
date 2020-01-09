659fc206cdd27edfb78310094a2c6bab4c8137ea
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-01-09 04:26:14 +0100

    Create 01. interfejs.java

---
+++ 08.01.2020/Welcomes S/01. interfejs.java
@@ -0,0 +1,11 @@
+package strategyPattern;
+
+public interface interfejs {
+
+    public String createHead();
+    public String createBody();
+    public String createFooter();
+
+    public String execute();
+
+}
