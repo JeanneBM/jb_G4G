017aa44638c74855961c641379267105d5cdca1e
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-01-09 04:30:48 +0100

    Create 09. ftocPolishWelcomeTemplateMethodTest.java

---
+++ 08.01.2020/Welcomes S/09. ftocPolishWelcomeTemplateMethodTest.java
@@ -0,0 +1,26 @@
+import strategyPattern.ftocPolishWelcomeTemplateMethod;
+import strategyPattern.interfejs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+    public class ftocPolishWelcomeTemplateMethodTest {
+
+        @Test
+        public void createHead_ShouldReturn_DeutschWelcome() {
+            interfejs mns = new ftocPolishWelcomeTemplateMethod();
+            Assert.assertEquals("Czesc! ", mns.createHead());
+        }
+        @Test
+        public void createBody_ShouldReturn_DeutschWelcome() {
+            interfejs mns = new ftocPolishWelcomeTemplateMethod();
+            Assert.assertEquals("Mam na imie Asia. ", mns.createBody());
+        }
+
+        @Test
+        public void createFooter_ShouldReturn_DeutschWelcome() {
+            interfejs mns = new ftocPolishWelcomeTemplateMethod();
+            Assert.assertEquals("Do zobaczenia!", mns.createFooter());
+        }
+
+    }
