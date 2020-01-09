dc4bf3abba8aaec8c964f6c3f2772776e597dfe5
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-01-09 04:26:47 +0100

    Create 02. ftocDeutschWelcomeTemplateMethod.java

---
+++ 08.01.2020/Welcomes S/02. ftocDeutschWelcomeTemplateMethod.java
@@ -0,0 +1,29 @@
+package strategyPattern;
+
+public class ftocDeutschWelcomeTemplateMethod implements interfejs {
+    @Override
+    public String createHead()
+    {
+        System.out.print("Guten Tag! ");
+        return "Guten Tag! ";
+
+    }
+    @Override
+    public String createBody()
+    {
+        System.out.print("Ich heiße Asia. ");
+        return "Ich heiße Asia. ";
+    }
+    @Override
+    public String createFooter()
+    {
+        System.out.println("Auf Wiedersehen!");
+        return "Auf Wiedersehen!";
+    }
+    @Override
+    public String execute()
+    {
+    return createHead() + createBody() + createFooter();
+    }
+}
+
