961310bd521050a08e96b270fd389f2264280002
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-01-09 04:27:19 +0100

    Create 03. ftocPolishWelcomeTemplateMethod.java

---
+++ 08.01.2020/Welcomes S/03. ftocPolishWelcomeTemplateMethod.java
@@ -0,0 +1,18 @@
+package strategyPattern;
+
+public class ftocPolishWelcomeTemplateMethod implements interfejs {
+    @Override
+    public String createHead() {       System.out.print ("Czesc! "); return "Czesc! "; }
+
+
+    @Override
+    public String createBody() {       System.out.print("Mam na imie Asia. "); return "Mam na imie Asia. "; }
+
+    @Override
+    public String createFooter() {     System.out.println("Do zobaczenia!"); return "Do zobaczenia!";    }
+    @Override
+    public String execute()
+    {
+        return createHead() + createBody() + createFooter();
+    }
+}
