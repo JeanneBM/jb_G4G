097dbb0d01ff834f80eb67986b0fdc85aa838578
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-02-05 20:03:56 +0100

    Create 02. View.java

---
+++ Java Design Patterns and Architecture Tutorial/MVC/Project/02. View.java
@@ -0,0 +1,8 @@
+package mvc;
+
+public class View {
+    public void printWelcomeDetails (String createHead, String createBody, String createFooter) {
+        System.out.println(createHead+createBody+createFooter);
+
+    }
+}
