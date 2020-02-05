b480c832d2e0fa075b326c15c966e3fc55ab2486
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-02-05 20:03:25 +0100

    Create 01. Model.java

---
+++ Java Design Patterns and Architecture Tutorial/MVC/Project/01. Model.java
@@ -0,0 +1,31 @@
+package mvc;
+
+public class Model {
+    private String createHead;
+    private String createBody;
+    private String createFooter;
+
+    public String getCreateHead() {
+        return createHead;
+    }
+
+    public void setCreateHead(String createHead) {
+        this.createHead=createHead;
+    }
+
+    public String getCreateBody() {
+        return createBody;
+    }
+
+    public void setCreateBody(String createBody) {
+        this.createBody=createBody;
+    }
+
+    public String getCreateFooter() {
+        return createFooter;
+    }
+
+    public void setCreateFooter(String createFooter) {
+        this.createFooter=createFooter;
+    }
+}
