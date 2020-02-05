8ec513977823f637ceb30126abb41b469d42b83f
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-02-05 22:49:32 +0100

    Update 03. Controller.java

--- Java Design Patterns and Architecture Tutorial/MVC/Pattern/CaveofProgramming/03. Controller.java
+++ Java Design Patterns and Architecture Tutorial/MVC/Pattern/CaveofProgramming/03. Controller.java
@@ -1,7 +1,3 @@
-package com.caveofprogramming.designpatterns.demo1.controller;
-
-import com.caveofprogramming.designpatterns.demo1.model.Model;
-import com.caveofprogramming.designpatterns.demo1.view.View;
 
 public class Controller {
 	private View view;
