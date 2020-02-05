a6c45ee59ea63bacd3b43e1283d27505a38b9033
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-02-05 20:57:44 +0100

    Create 03. Controller.java

---
+++ Java Design Patterns and Architecture Tutorial/Observer/Project/03. Controller.java
@@ -0,0 +1,11 @@
+public class Controller {
+    private View view;
+    private Model model;
+
+    public Controller(View view, Model model) {
+        this.view = view;
+        this.model = model;
+    }
+
+}
+
