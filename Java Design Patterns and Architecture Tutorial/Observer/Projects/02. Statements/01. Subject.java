7ba9b829c288d9991e1f24de04c84bd464267a64
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-02-07 00:08:23 +0100

    Create 01. Subject.java

---
+++ Java Design Patterns and Architecture Tutorial/Observer/Projects/02. Statements/01. Subject.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+
+    private List<Observer> observers = new ArrayList<Observer>();
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String  state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+
+    public void attach(Observer observer){
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers(){
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
+
