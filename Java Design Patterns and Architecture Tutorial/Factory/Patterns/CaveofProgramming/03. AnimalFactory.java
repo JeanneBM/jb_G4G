5e43d7ccca7167db72b44506b6cdc78a371474cd
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-02-13 23:47:54 +0100

    Create 03. AnimalFactory.java

---
+++ Java Design Patterns and Architecture Tutorial/Factory/Patterns/CaveofProgramming/03. AnimalFactory.java
@@ -0,0 +1,17 @@
+public abstract class AnimalFactory {
+	
+	public static final int CAT = 0;
+	public static final int DOG = 1;
+	
+	public static Animal createAnimal(int type) {
+		
+		switch(type) {
+		case CAT:
+			return new Cat();
+		case DOG: 
+			return new Dog();
+		}
+		
+		return null;
+	}
+}
