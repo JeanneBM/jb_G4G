c4a69a2ceb30a6641be3081b666caeaa60164d6a
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-02-13 23:44:02 +0100

    Create 04. GameConsole.java

---
+++ Java Design Patterns and Architecture Tutorial/Facade/Patterns/CaveofProgramming/04. GameConsole.java
@@ -0,0 +1,9 @@
+public class GameConsole {
+	public void clearScreen() {
+		System.out.println("Clearing screen ....");
+	}
+	
+	public void draw(GameObjects objects) {
+		System.out.println("Drawing ....");
+	}
+}
