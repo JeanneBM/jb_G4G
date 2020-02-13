6f5b6b946143619202020020bf14e8f138cbd068
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-02-13 23:19:37 +0100

    Create 05. Database.java

---
+++ Java Design Patterns and Architecture Tutorial/Beans/05. Database.java
@@ -0,0 +1,61 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+	
+	private static Database instance = new Database();
+	
+	private Connection con;
+	
+	private Database() {
+		
+	}
+	
+	public static Database getInstance() {
+		return instance;
+	}
+	
+	/*
+	private static Database instanceOld;
+	
+	public static Database getInstanceOld() {
+		if(instanceOld == null) {
+			instanceOld = new Database();
+		}
+		
+		return instanceOld;
+	}
+	*/
+	
+	/*
+	 * Add whatever methods you like to your singleton class.
+	 */
+	public void connect() throws Exception {
+		if (con != null)
+			return;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new Exception("Driver not found");
+		}
+
+		String url = String.format("jdbc:mysql://localhost:%d/patterns", 3306);
+
+		con = DriverManager.getConnection(url, "squiffy", "letmein");
+	}
+	
+	public void disconnect() {
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Can't close connection");
+			}
+		}
+		
+		con = null;
+	}
+	
+}
