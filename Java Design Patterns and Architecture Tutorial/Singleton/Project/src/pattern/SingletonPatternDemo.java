92cceb596d0851610bf825b13a6a413922856dae
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-03-07 22:53:15 +0100

    Update SingletonPatternDemo.java

--- Java Design Patterns and Architecture Tutorial/Singleton/Project/src/pattern/SingletonPatternDemo.java
+++ Java Design Patterns and Architecture Tutorial/Singleton/Project/src/pattern/SingletonPatternDemo.java
@@ -1,16 +1,10 @@
 public class SingletonPatternDemo {
     public static void main(String[] args) {
 
-        //illegal construct
-        //Compile Time Error: The constructor SingleObject() is not visible
-        //SingleObject object = new SingleObject();
-
-        //Get the only object available
         DeutschWelcome german_object = DeutschWelcome.getInstance();
         PolishWelcome polish_object = PolishWelcome.getInstance();
         SpanishWelcome spanish_object = SpanishWelcome.getInstance();
 
-        //show the message
         german_object.showMessage();
         polish_object.showMessage();
         spanish_object.showMessage();
