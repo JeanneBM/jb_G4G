3df0286678eb5eb86f22b753d5041fd352062ffa
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-03-07 22:53:59 +0100

    Update and rename  PolishWelcome.java to PolishWelcome.java

--- Java Design Patterns and Architecture Tutorial/Singleton/Project/src/pattern/PolishWelcome.java
+++ Java Design Patterns and Architecture Tutorial/Singleton/Project/src/pattern/PolishWelcome.java
@@ -1,11 +1,8 @@
 public class PolishWelcome {
     private static PolishWelcome instance = new PolishWelcome();
-
-    //make the constructor private so that this class cannot be
-    //instantiated
+    
     private PolishWelcome(){}
 
-    //Get the only object available
     public static PolishWelcome getInstance(){
         return instance;
     }
