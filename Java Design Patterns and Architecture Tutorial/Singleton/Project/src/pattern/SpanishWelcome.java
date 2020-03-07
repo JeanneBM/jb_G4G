a8df07692d7a29732754993237959afe65409ae7
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-03-07 22:54:24 +0100

    Update and rename  SpanishWelcome.java to SpanishWelcome.java

--- Java Design Patterns and Architecture Tutorial/Singleton/Project/src/pattern/SpanishWelcome.java
+++ Java Design Patterns and Architecture Tutorial/Singleton/Project/src/pattern/SpanishWelcome.java
@@ -1,11 +1,8 @@
 public class SpanishWelcome {
     private static SpanishWelcome instance = new SpanishWelcome();
-
-    //make the constructor private so that this class cannot be
-    //instantiated
+    
     private SpanishWelcome(){}
 
-    //Get the only object available
     public static SpanishWelcome getInstance(){
         return instance;
     }
