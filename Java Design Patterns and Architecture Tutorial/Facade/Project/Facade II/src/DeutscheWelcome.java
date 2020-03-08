3e2b3ef56fe1df88a900c16c0239a61263c7c6ee
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-03-08 23:25:21 +0100

    Create DeutscheWelcome.java

---
+++ Java Design Patterns and Architecture Tutorial/Facade/Project/Facade II/src/DeutscheWelcome.java
@@ -0,0 +1,7 @@
+package pattern;
+
+public class DeutscheWelcome {
+    public void update(InputSystem userInput) {
+        System.out.println("Guten Tag! Ich heiße Asia. Auf Wiedersehen!");
+    }
+}
