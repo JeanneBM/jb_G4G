905e50cf8cbc37730b23429f180a654abbc1a205
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-03-08 22:40:16 +0100

    Create SpanishWelcome.java

---
+++ Java Design Patterns and Architecture Tutorial/Facade/Project/src/SpanishWelcome.java
@@ -0,0 +1,9 @@
+package pattern;
+
+public class SpanishWelcome implements interfejs {
+
+    @Override
+    public void welcomeMessage() {
+        System.out.println("Hola! Mi nombre es Asia. Hasta La Vista!");
+    }
+}
