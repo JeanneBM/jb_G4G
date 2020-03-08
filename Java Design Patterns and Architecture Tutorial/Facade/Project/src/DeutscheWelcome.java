0f9b8aff96c91c0dc8334b6abae72e79657e108a
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-03-08 22:39:00 +0100

    Create DeutscheWelcome.java

---
+++ Java Design Patterns and Architecture Tutorial/Facade/Project/src/DeutscheWelcome.java
@@ -0,0 +1,10 @@
+package pattern;
+
+    public class DeutscheWelcome implements interfejs {
+
+        @Override
+        public void welcomeMessage() {
+            System.out.println("Guten Tag! Ich heiße Asia. Auf Wiedersehen!");
+        }
+    }
+
