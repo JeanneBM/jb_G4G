20fd834c9d3d140c8f44c8c87ba60b50f1d231b7
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-03-08 22:39:33 +0100

    Create PolishWelcome.java

---
+++ Java Design Patterns and Architecture Tutorial/Facade/Project/src/PolishWelcome.java
@@ -0,0 +1,9 @@
+package pattern;
+
+public class PolishWelcome implements interfejs {
+
+    @Override
+    public void welcomeMessage() {
+        System.out.println("Czesc! Mam na imie Asia. Do zobaczenia!");
+    }
+}
