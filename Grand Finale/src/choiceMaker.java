9686b07ad4e6aa05766d5e89e2744951969facbc
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-04-15 00:01:42 +0200

    Update choiceMaker.java

--- Grand Finale/src/choiceMaker.java
+++ Grand Finale/src/choiceMaker.java
@@ -1,1 +1,14 @@
+public class choiceMaker {
+    static String welcomeType="";
+    public static void main(String[] args) {
+        ChoiceTaker choiceTaker = new ChoiceTaker();
 
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("In what range is the first letter of the language you are looking for?");
+            System.out.println("(1) A-G");
+            System.out.println("(2) H-N");
+            System.out.println("(Other) Libertà");
+
+            int a = scanner.nextInt();
+            if(a==1) {
