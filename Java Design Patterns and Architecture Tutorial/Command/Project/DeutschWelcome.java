d6ed2cf75793a645259342baea36fef597a583d4
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-04-15 23:49:37 +0200

    Create DeutschWelcome.java

---
+++ Java Design Patterns and Architecture Tutorial/Command/Project/DeutschWelcome.java
@@ -0,0 +1,10 @@
+public class DeutschWelcome {
+
+    private String deutsche = "Guten Tag! Ich heiße Asia. Auf Wiedersehen!\n";
+
+    public void greetD(){
+        System.out.println("Welcome: " + deutsche);
+    }
+
+}
+
