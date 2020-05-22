22c9ffa971f07e256cf71b657802d39a9c868eed
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-05-22 23:18:06 +0200

    Create Arabski.java

---
+++ Grand Finale/src/Welcomes/AG/Arabski.java
@@ -0,0 +1,11 @@
+package AG;
+
+public class Arabski extends AG {
+    public arabski() {
+    }
+
+    public String getWelcome() {
+        return "صباح الخير! اسمي آسيا. أراك لاحقًا!// sabah alkhyr! aismi asia. 'arak lahqana!";
+    }
+}
+
