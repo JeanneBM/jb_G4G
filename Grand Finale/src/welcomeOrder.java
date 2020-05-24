4d4a033f960b2cf380171e33a070238bd292c7ff
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-05-24 23:50:28 +0200

    Update welcomeOrder.java

--- Grand Finale/src/welcomeOrder.java
+++ Grand Finale/src/welcomeOrder.java
@@ -1,22 +1,3 @@
-import Welcome.*;
-
-public class welcomeOrder {
-
-    public Welcome makeWelcome(String welcomeType) {
-        if (welcomeType.equals(null)){
-            return null;
-        }
-        else if(welcomeType.equals("Afrikaans")){
-            return new Afrikaans();
-        }
-        else if(welcomeType.equals("Arabski")){
-            return new Arabski();
-        }
-        else if(welcomeType.equals("Bengalski")){
-            return new Bengalski();
-        }
-
-        return null;
 
         
         
