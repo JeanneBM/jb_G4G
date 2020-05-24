a654ac0d75a34929859071aa5e59b900e5735abd
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-05-24 23:54:34 +0200

    Update welcomeOrder.java

--- Grand Finale/src/welcomeOrder.java
+++ Grand Finale/src/welcomeOrder.java
@@ -1,3 +1,16 @@
+public class welcomeOrder implements interfejsCommand{
+    welcomeMaker WelcomeMaker;
+    String order;
+    public welcomeOrder(welcomeMaker WelcomeMaker, String order){
+        this.WelcomeMaker = WelcomeMaker;
+        this.order= order;
+    }
 
+    @Override
+    public void orderUp() {
+        WelcomeMaker.welcomeType = order;
+        WelcomeMaker.getOrder("Welcome");
+    }
+}
         
         
