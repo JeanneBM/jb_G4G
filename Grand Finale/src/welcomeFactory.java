23a04d17db5b378815d08fababbe4858bdfea13b
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-05-24 23:50:44 +0200

    Update welcomeFactory.java

--- Grand Finale/src/welcomeFactory.java
+++ Grand Finale/src/welcomeFactory.java
@@ -1,1 +1,20 @@
+import Welcome.*;
+
+public class welcomeOrder {
+
+    public Welcome makeWelcome(String welcomeType) {
+        if (welcomeType.equals(null)){
+            return null;
+        }
+        else if(welcomeType.equals("Afrikaans")){
+            return new Afrikaans();
+        }
+        else if(welcomeType.equals("Arabski")){
+            return new Arabski();
+        }
+        else if(welcomeType.equals("Bengalski")){
+            return new Bengalski();
+        }
+
+        return null;
 
