e50a1f263571ea059ed36cbcec87a93522826fe8
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-06-04 23:47:15 +0200

    Update choiceMaker.java

--- Grand Finale/src/choiceMaker.java
+++ Grand Finale/src/choiceMaker.java
@@ -1,3 +1,4 @@
+
 public class choiceMaker {
     static String welcomeType="";
     public static void main(String[] args) {
@@ -12,3 +13,58 @@ public class choiceMaker {
 
             int a = scanner.nextInt();
             if(a==1) {
+            	Scanner scanAG = new Scanner(System.in);
+                System.out.println("-------------------- (1) A-G --------------------");
+                for (int i = 0; i < 1; i++) {
+                    System.out.println("-------------------- Languages --------------------");
+                    System.out.println("(1) ");
+                    System.out.println("(2) ");
+                    System.out.println("(3) ");
+		    int welcome = scanAG.nextInt();
+		      if (welcome == 1) {
+                        welcomeType = "Afrikaans";
+                        System.out.println("(1) is chosen.");
+                    } else if (welcome == 2) {
+                        welcomeType = "Arabski";
+                        System.out.println("(2) is chosen.");
+                    } else if (welcome == 3) {
+                        welcomeType = "Bengalski";
+                        System.out.println("(3) is chosen.");
+			
+ 	    else if (a == 2) {
+                Scanner scanHN = new Scanner(System.in);
+                System.out.println("-------------------- (2) H-N --------------------");
+                for (int i = 0; i < 1; i++) {
+                    System.out.println("-------------------- Languages --------------------");
+                    System.out.println("(1) ");
+                    System.out.println("(2) ");
+                    System.out.println("(3) ");
+		    int welcome = scanHN.nextInt();
+		      if (welcome == 1) {
+                        welcomeType = "";
+                        System.out.println("(1) is chosen.");
+                    } else if (welcome == 2) {
+                        welcomeType = "";
+                        System.out.println("(2) is chosen.");
+                    } else if (welcome == 3) {
+                        welcomeType = "";
+                        System.out.println("(3) is chosen.");
+                    } else {
+                        break;
+                    }
+                }
+		
+		System.out.println("");
+                WelcomeOrder welcomeOrder = new welcomeOrder(WelcomeMaker, welcomeType);
+                choiceTaker.takeChoice(welcomeOrder);
+            } else {
+                System.out.println("-------------------------------------------------------------------------------");
+                System.out.println("Libertà");
+              break;
+            }
+        }
+    }
+}	
+            
+            
+            
