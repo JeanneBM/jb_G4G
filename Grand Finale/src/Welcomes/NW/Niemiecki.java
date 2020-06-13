eeecf6520fe21fc12a8a6bc24d9b1bef97f88f4d
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-06-13 23:50:25 +0200

    Update Niemiecki.java

--- Grand Finale/src/Welcomes/NW/Niemiecki.java
+++ Grand Finale/src/Welcomes/NW/Niemiecki.java
@@ -1,1 +1,8 @@
 Guten Morgen! Ich heiße Asia. Wir sehen uns!
+public class Niemiecki extends AbstractAG {
+
+    @Override
+    public String createWelcome() {
+        return "Guten Morgen! Ich heiße Asia. Wir sehen uns!";
+    }
+}
