fd99ee3a8ce45836e295e89f5b64010fcad99925
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-06-17 23:19:40 +0200

    Update welcomeMaker.java

--- Grand Finale/src/welcomeMaker.java
+++ Grand Finale/src/welcomeMaker.java
@@ -1,32 +1,28 @@
-import Welcomes.*;
-
-// Receiver
 public class welcomeMaker {
-        public static String WelcomeType="";
-     	interfejsAG InterfejsWelcomes;
-        public void getOrder(String factoryType){
-	
-	
-        if(factoryType.equalsIgnoreCase(anotherString: "(1) A-G")){
-		        
-        	System.out.println(WelcomeType + " order received.");
-        	welcomeFactory factory = new welcomeFactory();
-        	InterfejsWelcomes = factory.makeWelcome(WelcomeType);
-        	InterfejsWelcomes.createWelcome();
-       		System.out.println(InterfejsWelcomes.getWelcome() + " order completed.");
-	}
-	
-	
-	if(factoryType.equalsIgnoreCase(anotherString: "(2) H-N")){
-	
-		System.out.println(WelcomeType + " order received.");
-        	welcomeFactory factory = new welcomeFactory();
-        	InterfejsWelcomes = factory.makeWelcome(WelcomeType);
-       		InterfejsWelcomes.createWelcome();
-        	System.out.println(InterfejsWelcomes.getWelcome() + " order completed.");
-        }
-	
-        System.out.println("");
-}
+    public static String WelcomeType="";
 
-    
+    public void getOrder(String factoryType){
+
+
+        if(factoryType.equalsIgnoreCase(anotherString: "(1) A-G")){
+
+            System.out.println(WelcomeType + " order received.");
+            welcomeFactory factory = new welcomeFactory();
+            interfejsAG = factory.makeWelcome(WelcomeType);
+            interfejsAG.createWelcome();
+            System.out.println(interfejsAG.getWelcome() + " order completed.");
+        }
+
+
+	    if(factoryType.equalsIgnoreCase(anotherString: "(2) H-N")){
+
+            System.out.println(WelcomeType + " order received.");
+            welcomeFactory factory = new welcomeFactory();
+            interfejsAG = factory.makeWelcome(WelcomeType);
+            interfejsAG.createWelcome();
+            System.out.println(interfejsAG.getWelcome() + " order completed.");
+        }
+
+        System.out.println("");
+    }
+
