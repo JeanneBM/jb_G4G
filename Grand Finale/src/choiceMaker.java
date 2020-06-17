1c5ebadfab26b807fedc05963c04195e7c06b9a9
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-06-17 23:24:20 +0200

    Update choiceMaker.java

--- Grand Finale/src/choiceMaker.java
+++ Grand Finale/src/choiceMaker.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 
 public class choiceMaker {
     static String welcomeType="";
     public static void main(String[] args) {
-        ChoiceTaker choiceTaker = new ChoiceTaker();
+        choiceTaker ChoiceTaker = new choiceTaker();
 
         Scanner scanner = new Scanner(System.in);
         while(true) {
@@ -13,15 +14,15 @@ public class choiceMaker {
 
             int a = scanner.nextInt();
             if(a==1) {
-            	Scanner scanAG = new Scanner(System.in);
+                Scanner scanAG = new Scanner(System.in);
                 System.out.println("-------------------- (1) A-G --------------------");
                 for (int i = 0; i < 1; i++) {
                     System.out.println("-------------------- Languages --------------------");
                     System.out.println("(1) ");
                     System.out.println("(2) ");
                     System.out.println("(3) ");
-		    int welcome = scanAG.nextInt();
-		      if (welcome == 1) {
+                    int welcome = scanAG.nextInt();
+                    if (welcome == 1) {
                         welcomeType = "Afrikaans";
                         System.out.println("(1) is chosen.");
                     } else if (welcome == 2) {
@@ -31,63 +32,62 @@ public class choiceMaker {
                         welcomeType = "Bengalski";
                         System.out.println("(3) is chosen.");
 			
- 	    else if (a == 2) {
-                Scanner scanHN = new Scanner(System.in);
-                System.out.println("-------------------- (2) H-M --------------------");
-                for (int i = 0; i < 1; i++) {
-                    System.out.println("-------------------- Languages --------------------");
-                    System.out.println("(1) ");
-                    System.out.println("(2) ");
-                    System.out.println("(3) ");
-		    int welcome = scanHN.nextInt();
-		      if (welcome == 1) {
-                        welcomeType = "";
-                        System.out.println("(1) is chosen.");
-                    } else if (welcome == 2) {
-                        welcomeType = "";
-                        System.out.println("(2) is chosen.");
-                    } else if (welcome == 3) {
-                        welcomeType = "";
-                        System.out.println("(3) is chosen.");
-                    } else {
-                        break;
-                    }
-                }
+ 	    if (a == 2) {
+                            Scanner scanHN = new Scanner(System.in);
+                            System.out.println("-------------------- (2) H-M --------------------");
+                            for (int i = 0; i < 1; i++) {
+                                System.out.println("-------------------- Languages --------------------");
+                                System.out.println("(1) ");
+                                System.out.println("(2) ");
+                                System.out.println("(3) ");
+                                int welcome = scanHN.nextInt();
+                                if (welcome == 1) {
+                                    welcomeType = "";
+                                    System.out.println("(1) is chosen.");
+                                } else if (welcome == 2) {
+                                    welcomeType = "";
+                                    System.out.println("(2) is chosen.");
+                                } else if (welcome == 3) {
+                                    welcomeType = "";
+                                    System.out.println("(3) is chosen.");
+                                } else {
+                                    break;
+                                }
+                            }
 		
- 	    else if (a == 3) {
-                Scanner scanNW = new Scanner(System.in);
-                System.out.println("-------------------- (3) N-W --------------------");
-                for (int i = 0; i < 1; i++) {
-                    System.out.println("-------------------- Languages --------------------");
-                    System.out.println("(1) ");
-                    System.out.println("(2) ");
-                    System.out.println("(3) ");
-		    int welcome = scanHN.nextInt();
-		      if (welcome == 1) {
-                        welcomeType = "";
-                        System.out.println("(1) is chosen.");
-                    } else if (welcome == 2) {
-                        welcomeType = "";
-                        System.out.println("(2) is chosen.");
-                    } else if (welcome == 3) {
-                        welcomeType = "";
-                        System.out.println("(3) is chosen.");
-                    } else {
-                        break;
+ 	    if (a == 3) {
+                                Scanner scanNW = new Scanner(System.in);
+                                System.out.println("-------------------- (3) N-W --------------------");
+                                for (int i = 0; i < 1; i++) {
+                                    System.out.println("-------------------- Languages --------------------");
+                                    System.out.println("(1) ");
+                                    System.out.println("(2) ");
+                                    System.out.println("(3) ");
+                                    int welcome = scanHN.nextInt();
+                                    if (welcome == 1) {
+                                        welcomeType = "";
+                                        System.out.println("(1) is chosen.");
+                                    } else if (welcome == 2) {
+                                        welcomeType = "";
+                                        System.out.println("(2) is chosen.");
+                                    } else if (welcome == 3) {
+                                        welcomeType = "";
+                                        System.out.println("(3) is chosen.");
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                System.out.println("");
+                                welcomeOrder welcomeOrder = new welcomeOrder(welcomeMaker, welcomeType);
+                                ChoiceTaker.takeChoice(welcomeOrder);
+                            } else {
+                                System.out.println("-------------------------------------------------------------------------------");
+                                System.out.println("Libertà");
+                                break;
+                            }
+                        }
                     }
                 }
-		    
-		    System.out.println("");
-                WelcomeOrder welcomeOrder = new welcomeOrder(WelcomeMaker, welcomeType);
-                choiceTaker.takeChoice(welcomeOrder);
-            } else {
-                System.out.println("-------------------------------------------------------------------------------");
-                System.out.println("Libertà");
-              break;
-            }
-        }
-    }
-}	
-            
             
             
