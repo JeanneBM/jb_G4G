02618299a3e3591e4e5a139865b0b488abd2fc00
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-06-18 21:33:46 +0200

    Update welcomeMaker.java

--- Grand Finale/src/welcomeMaker.java
+++ Grand Finale/src/welcomeMaker.java
@@ -1,28 +1,29 @@
 public class welcomeMaker {
     public static String WelcomeType="";
 
+
+    interfejsAG InterfejsAG;
     public void getOrder(String factoryType){
-
-
-        if(factoryType.equalsIgnoreCase(anotherString: "(1) A-G")){
+           if(factoryType.equalsIgnoreCase(anotherString: "(1) A-G")){
 
             System.out.println(WelcomeType + " order received.");
             welcomeFactory factory = new welcomeFactory();
-            interfejsAG = factory.makeWelcome(WelcomeType);
-            interfejsAG.createWelcome();
-            System.out.println(interfejsAG.getWelcome() + " order completed.");
+            InterfejsAG = factory.makeWelcome(WelcomeType);
+            InterfejsAG.createWelcome();
+            System.out.println(InterfejsAG.getWelcome() + " order completed.");
         }
 
 
-	 if(factoryType.equalsIgnoreCase(anotherString: "(2) H-N")){
+	    if(factoryType.equalsIgnoreCase(anotherString: "(2) H-N")){
 
             System.out.println(WelcomeType + " order received.");
             welcomeFactory factory = new welcomeFactory();
-            interfejsAG = factory.makeWelcome(WelcomeType);
-            interfejsAG.createWelcome();
-            System.out.println(interfejsAG.getWelcome() + " order completed.");
+            InterfejsAG = factory.makeWelcome(WelcomeType);
+            InterfejsAG.createWelcome();
+            System.out.println(InterfejsAG.getWelcome() + " order completed.");
         }
 
         System.out.println("");
     }
-
+	
+	
