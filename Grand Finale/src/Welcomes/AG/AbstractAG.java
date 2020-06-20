06facbc8f27c0d8caefcbe33766be481b7810226
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-06-20 23:53:28 +0200

    Update AbstractAG.java

--- Grand Finale/src/Welcomes/AG/AbstractAG.java
+++ Grand Finale/src/Welcomes/AG/AbstractAG.java
@@ -1,1 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public abstract class AbstractAG implements interfejsAG {
+        public abstract String getWelcome();
+
+        public void createWelcome(){
+
+
+        if (opcja1()) {
+        makingopcja1();
+    }
+}
+
+    public void makingopcja1() {
+        System.out.println("We are making opcja1! ");
+    }
+
+    //hook
+    public boolean opcja1() {
+        String answer = getUserInput();
+
+        if (answer.toLowerCase().startsWith("y")) {
+            return true;
+        } else
+            return false;
+    }
+
+    private String getUserInput() {
+        String answer = null;
+        System.out.println("Would you like opcja1? (y/n)");
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            answer = input.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (answer == null) {
+            answer = "no";
+        }
+
+        return answer;
+    }
+}
 
